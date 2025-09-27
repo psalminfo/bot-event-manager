14a15aa0ff221796f9123cffb94ca818991e8b88
psalminfo <psalm4all@gmail.com>
2025-09-27 23:22:36 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  experimental: {
+    appDir: true,
+  },
+}
+
+module.exports = nextConfig
